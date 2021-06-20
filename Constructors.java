5186b55236049e5ed6459c2bd7137231890a8fac
KattachaithanyaKumar <80614118+KattachaithanyaKumar@users.noreply.github.com>
2021-06-20 13:43:26 +0530

    Update Constructors.java

--- Constructors.java
+++ Constructors.java
@@ -1,22 +1,26 @@
 class A {
+    //this is a normal constructor with no parameters 
     A()  {
         System.out.println("A: constructor");
     }
 }
 
 class B {
+    //this is a parameterized constructor where we are taking int a when the object is created and we can use that value in the code.
     B(int a)  {
         System.out.println("B: constructor with " + a);
     }
 }
 
 class C {
+    //similarly this is a parameterized constructor with two parameters, which are taken when the obect is created.
     C(int a, int b)  {
         System.out.println("C: constructor with " + a + " and " + b);
     }
 }
 
 class D{
+    //similarly this is a parameterized constructor with two different parameters with different data types.
     D(String name, int age)  {
         System.out.println("D: constructor with " + name + " and " + age);
     }
