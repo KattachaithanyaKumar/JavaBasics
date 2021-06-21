62e019eeaa4611321fa31d3e838177ef15b3b686
KattachaithanyaKumar <80614118+KattachaithanyaKumar@users.noreply.github.com>
2021-06-21 22:10:29 +0530

    Update VariableTypes.java

--- VariableTypes.java
+++ VariableTypes.java
@@ -7,11 +7,15 @@ public class VariableTypes {
         char c = 'c';
         boolean bool = true;
         String s = "Hello";
+        short sh = 10000;
+        long lo = 30000;
 
         a = 50;
         b = 40;
         System.out.println("int a: " + a + " b: " + b);
         System.out.println("int x: " + x + " y: " + y);
+        System.out.println("short: " + sh);
+        System.out.println("long: " + lo);
         System.out.println("Byte: " + by);
         System.out.println("double: " + d);
         System.out.println("char: " + c);
